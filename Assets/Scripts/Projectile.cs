1aa18c006576647f7f3a10952ceb467185fc2f7f
Allen Adams <allenofadams@hotmail.com>
2018-04-25 13:58:44 -0400

    First pass on gunfire.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    float LifeTime = 3;
+    float EndTime;
+
+    public float Speed = 25;
+
+    private void Start()
+    {
+        EndTime = Time.time + LifeTime;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Time.time < EndTime)
+        {
+            transform.Translate(transform.up * Speed * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            DestroyImmediate(gameObject);            
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public Transform Muzzle;
+    public GameObject AmmoPrefab;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Instantiate(AmmoPrefab, Muzzle.transform.position, Muzzle.transform.rotation);
+        }
+    }
+}
