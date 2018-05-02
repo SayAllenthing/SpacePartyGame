c2b87f885cae2501c29cf6b1d6b239e42044ac23
SayAllenthing <allenofadams@hotmail.com>
2018-05-01 22:06:20 -0400

    Work on room building.

--- Assets/Editor/RoomManagerEditor.cs
+++ Assets/Editor/RoomManagerEditor.cs
@@ -6,6 +6,8 @@ using UnityEditor;
 [CustomEditor(typeof(RoomManager))]
 public class RoomManagerEditor : Editor
 {
+    public bool EditMode = true;
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
@@ -32,6 +34,39 @@ public class RoomManagerEditor : Editor
             foreach(ModularContact c in contacts)
             {
                 c.gameObject.SetActive(!c.gameObject.activeSelf);
+            }
+
+            EditMode = !EditMode;
+        }
+    }
+
+    private void OnSceneGUI()
+    {
+        if (!EditMode)
+            return;
+
+        RoomManager component = (RoomManager)target;
+
+        ModularContact[] contacts = component.GetComponentsInChildren<ModularContact>();
+
+        foreach(ModularContact contact in contacts)
+        {
+            if(contact.Contact == null)
+            {
+                Handles.color = Color.red;
+                if (Handles.Button(contact.transform.position, Quaternion.LookRotation(-contact.transform.forward), 1.25f, 2, Handles.ArrowHandleCap))
+                {
+                    contact.GenerateFloor();
+                }
+
+                Handles.color = Color.blue;
+                if (Handles.Button(contact.transform.position, Quaternion.LookRotation(contact.transform.up), 1.25f, 2, Handles.ArrowHandleCap))
+                {
+                    string[] paths = AssetDatabase.FindAssets("t:WallObjectDataBase");
+                    WallObjectDatabase database = (WallObjectDatabase)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(paths[0]), typeof(WallObjectDatabase));
+
+                    contact.GenerateWall(database);
+                }
             }
         }
     }
